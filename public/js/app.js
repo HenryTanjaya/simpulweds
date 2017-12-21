f1c6635a7de191b973151a6efaaa22ba037d3e23
HenryTanjaya <henrytanjaya11@gmail.com>
2017-12-21 16:26:59 +0700

    revision

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,26 @@
 
 // alert("Hello");
-$(window).on("scroll", function() {
-    if($(window).scrollTop() > 700) {
-      $("a").addClass("active");
-      // $("#logo").attr("src","/images/logo.jpg");
-    } else {
-      $("a").removeClass("active");
-      // $("#logo").attr("src","/images/logowhite.jpg");
-      // alert("white")
-        //remove the background property so it comes transparent again (defined in your css)
-        // $("header").removeClass("active");
+
+$(document).ready(
+  $(window).on("scroll", function() {
+    if ($(window).width() < 768) {
+        if($(window).scrollTop() > 500) {
+          $(".navbar-dark .navbar-nav .nav-link").addClass("active");
+        } else if ($(window).scrollTop() > 150){
+          $(".logo-signature h6").addClass("active")
+        } else {
+          $(".navbar-dark .navbar-nav .nav-link").removeClass("active");
+          $(".logo-signature h6").removeClass("active")
+        }
+    } else if ($(window).width() > 768){
+      if($(window).scrollTop() > 700) {
+        $(".navbar-dark .navbar-nav .nav-link").addClass("active");
+      } else if ($(window).scrollTop() > 200){
+        $(".logo-signature h6").addClass("active")
+      } else {
+        $(".navbar-dark .navbar-nav .nav-link").removeClass("active");
+        $(".logo-signature h6").removeClass("active")
+      }
     }
-});
+  })
+)
